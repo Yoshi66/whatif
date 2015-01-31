7724832569ddb4c3b05b4b45799df4ea0776a88a
Yoshiki <yoshiki.mc@gmail.com>
2015-01-31 13:52:31 -0800

    bootwatch introduced

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,7 @@ Rails.application.routes.draw do
     :registrations => "users/registrations"
   }
   get 'home/index'
-
   get 'home/show'
-
   root 'home#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
